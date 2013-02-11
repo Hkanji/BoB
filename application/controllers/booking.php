f6122a567578b7f444f8ebc2f3690a07cf2ca79e
itei-gaurav <itei.test2@gmail.com>
2013-02-11 17:21:09 +0530

    booking controller

--- application/controllers/booking.php
+++ application/controllers/booking.php
@@ -6,16 +6,22 @@ class Booking extends CI_Controller {
 	public function __construct()
 	{
 		parent::__construct();
+		
 		$this->load->database();
 		$this->load->library('session');
 		$this->load->model('booking_model');
 		$this->load->helper('url');
 		$this->load->helper('booking');
-		
+		$this->load->library('Zend/Loader');
+		ini_set('include_path',   ini_get('include_path') . PATH_SEPARATOR . APPPATH .'libraries');
+		Zend_Loader::loadClass('Zend_Gdata');
+		Zend_Loader::loadClass('Zend_Gdata_ClientLogin');
+		Zend_Loader::loadClass('Zend_Gdata_Calendar');
 	}
 
 	public function index()
 	{
+
 		$this->load->view('common/header');
 		$this->load->view('appointment/booking');
 		$this->load->view('common/footer');
@@ -40,12 +46,14 @@ class Booking extends CI_Controller {
 				}
 			}
 
-			$this->checkGoogleCalendar();
+			$this->checkGoogleCalendar($input_data);
+
 			$availibility=$this->checkAvailibility($input_data);
+			
 			if(is_numeric($availibility) && $availibility!=0) {
 				$reservation_id=$this->addReservation($input_data,$availibility);
-				$this->updateGoogleCalendar();
-				//$this->sendBookingEmail($reservation_id,$customer_email);
+				$this->updateGoogleCalendar($input_data);
+				$this->sendBookingEmail($reservation_id,$customer_email);
 				$data_array=array('status'=>'success','message'=>'Booking successfull','reservation_id'=>$reservation_id);
 				echo(arrayToJson($data_array));
 			}
@@ -71,21 +79,115 @@ class Booking extends CI_Controller {
 		return (true);
 	}
 	
-	function updateGoogleCalendar()
+	function updateGoogleCalendar($input_data)
 	{
-		$this->deleteReservation();
+		$this->deleteCalendarReservationData($input_data);
+		$this->addCalendarReservationData($input_data);
+	}
+	function addCalendarReservationData($input_data)
+	{
+	
+		$booking_data=$this->booking_model->getBookingDataByDate($input_data['booking_date'],$input_data['store_id']);
+
+		$client=$this->authenticateGoogle($input_data);
+		$store_name=$this->booking_model->getStoreName($input_data['store_id']);
+		foreach ($booking_data as $key=>$value) {
+			
+			$table_name=$this->booking_model->getTableName($value['capacity_id']);
+			$title='Table # '.$table_name.' booked by '.$value['customer_name'];
+			$description='Table # '.$table_name.', booked by '.$value['customer_name'].' for '.$value['party_size'].' people. Reservation #'.$value['id'];
+			$event_href=$this->createEvent($client,$title,$description,$store_name,$value['appointment_date'],strftime('%H:%M:%S', strtotime($value['appointment_start_time_stamp'])),$value['appointment_date'],strftime('%H:%M:%S',strtotime($value['appointment_end_time_stamp'])));
+
+			$this->booking_model->updateEventHref($value['id'],$event_href);
+		}
 	}
 
-	function checkGoogleCalendar()
+	function createEvent ($client, $title = '',
+		$desc='', $where = '',
+		$startDate = '', $startTime = '',
+		$endDate = '', $endTime = '', $tzOffset = '+05:30')
+		{
+		  $gdataCal = new Zend_Gdata_Calendar($client);
+		  $newEvent = $gdataCal->newEventEntry();
+
+		  $newEvent->title = $gdataCal->newTitle($title);
+		  $newEvent->where = array($gdataCal->newWhere($where));
+		  $newEvent->content = $gdataCal->newContent("$desc");
+
+		  $when = $gdataCal->newWhen();
+
+		  $when->startTime = "{$startDate}T{$startTime}.000{$tzOffset}";
+		   $when->endTime = "{$endDate}T{$endTime}.000{$tzOffset}";
+
+		  $newEvent->when = array($when);
+
+		  $createdEvent = $gdataCal->insertEvent($newEvent);
+		  return $eventEditUrl = $createdEvent->getLink('edit')->href;
+
+		}
+
+
+	function deleteCalendarReservationData($input_data)
 	{
+		$booking_data=$this->booking_model->getBookingDataByDate($input_data['booking_date'],$input_data['store_id']);	
+		$client=$this->authenticateGoogle($input_data);
+		if(count($booking_data)>0) {
+			foreach ($booking_data as $key=>$value) {
+				if($value['event_href']!='') {
+					$gdataCal = new Zend_Gdata_Calendar($client);
+					$gdataCal->delete($value['event_href']);					
+				}
+
+			}				
+		}
 		
+	}
+
+	function checkGoogleCalendar($input_data)
+	{
+
+			$client=$this->authenticateGoogle($input_data);
+
+			$gdataCal = new Zend_Gdata_Calendar($client);
+			$query = $gdataCal->newEventQuery();
+			$query->setUser('default');
+			$query->setVisibility('private');
+			$query->setProjection('full');
+			$query->setOrderby('starttime');
+			$query->setStartMin(strftime("%Y-%m-%d",strtotime($input_data['booking_date'])));
+			$query->setStartMax(strftime("%Y-%m-%d",strtotime("+1 day",strtotime($input_data['booking_date']))));
+
+			$eventFeed = $gdataCal->getCalendarEventFeed($query);
+			$booking_data_href='';
+
+			if(count($eventFeed)>0) {
+				foreach ($eventFeed as $event) {
+
+					$booking_data_href .= "'".$event->getLink('edit')->href."',";
+				
+				}
+				
+				$deletedEvents = $this->booking_model->getDeletedEvents(rtrim($booking_data_href,','),$input_data['booking_date']);
+			}
+			else {
+				$deletedEvents = $this->booking_model->getDeletedEvents('',$input_data['booking_date']);				
+			}
+			
+			if(count($deletedEvents)>0) {
+					foreach ($deletedEvents as $key=>$value) {
+						$this->booking_model->deleteData('allocation_master',array('id'=>$value['id']));
+						$this->booking_model->deleteData('allocation_details_master',array('allocation_id'=>$value['id']));
+					}
+				}
+
 	}
 
 	function sendBookingEmail($reservation_id,$customer_email)
 	{
 		$this->load->library('email');
+		$this->email->initialize(array('mailtype'=>'html'));
 
-		$this->email->from('admin@booking.com', 'Admin');
+		$this->email->from(FROM_EMAIL, 'Admin');
 		$this->email->to($customer_email);
 		
 		$this->email->subject('Reservation sucessfull');
@@ -94,12 +196,11 @@ class Booking extends CI_Controller {
 
 		$this->email->send();	
 		
-		$this->email->to("staff@booking.com");
+		$this->email->to(STAFF_EMAIL);
 		
 		$this->email->subject('New Reservation');
 		$this->email->message('A booking is made on the site<br />
 								The reservation number is: '.$customer_email);
-
 		$this->email->send();
 	}
 
@@ -108,14 +209,26 @@ class Booking extends CI_Controller {
 		return ($this->booking_model->checkAvailibility($input_data));
 	}
 
-	function addReservation()
+	function addReservation($input_data,$availibility)
 	{
-		return ($this->saveBookingData($input_data,$availibility));
+		return ($this->booking_model->saveBookingData($input_data,$availibility));
 	}
 
-	function deleteReservation()
-	{
 
+	function authenticateGoogle($input_data)
+	{
+		if($input_data['store_id']==1) {
+			$username=STORE_1_USERNAME;
+			$password=STORE_1_PASSWORD;
+		}
+		else {
+			$username=STORE_2_USERNAME;
+			$password=STORE_2_PASSWORD;		
+		}
+		
+		$service = Zend_Gdata_Calendar::AUTH_SERVICE_NAME; // predefined service name for calendar
+		$client = Zend_Gdata_ClientLogin::getHttpClient($username,$password,$service);		
+		return $client;			
 	}
 
 }
